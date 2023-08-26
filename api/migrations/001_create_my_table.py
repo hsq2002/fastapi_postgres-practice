2b07d71ccab0f7d7fe4163545c9d4f3b54b3549d
farellfebriano <farellfebriano8@gmail.com>
2023-08-26 13:36:56 -0500

    fix all the tabble error

--- api/migrations/001_create_my_table.py
+++ api/migrations/001_create_my_table.py
@@ -4,6 +4,7 @@ steps=[
         CREATE TABLE vacations(
         id SERIAL PRIMARY KEY NOT NULL,
         name VARCHAR(1000) NOT NULL,
+        from_date DATE NOT NULL,
         to_date DATE NOT NULL,
         thoughts TEXT
         );
